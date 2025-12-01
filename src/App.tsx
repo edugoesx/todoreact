a6b94bae558fc46a14615baba56b6f56be01cdee
edugoesx <edu.goes10@gmail.com>
2025-12-01 14:56:09 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+import { useState, type FormEvent } from "react";
+import TodoForm from "./components/TodoForm"
+import TodoHeader from "./components/TodoHeader"
+import TodoList from "./components/TodoList"
+import { TodoContainer } from "./components/TodoContainer";
+
+ export interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+
+
+
+function App() {
+    const [todoList, setTodoList] = useState<Todo[]>([]);
+    const [filter, setFilter] = useState<"all" | "active" | "completed" >("all");
+
+  const addTodo = (event: FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const todoItem = formData.get("todo") as string;
+
+    if(!todoItem.trim()) return
+
+    setTodoList((prev) => [...prev, {
+      id: Date.now(),
+      text: todoItem,
+      completed: false
+    },
+  ]);
+
+
+    event.currentTarget.reset();
+  }
+
+
+    const toggleTodoCompleted = (id: number) => {
+      const newTodoList = todoList.map(todo => {
+        if(id === todo.id) {
+          
+          const completed = !todo.completed;
+          
+          return {
+            ...todo,
+            completed,
+          };
+        }
+
+        return todo;
+      });
+
+      setTodoList(newTodoList);
+    };
+  
+
+    const filteredTodos = todoList.filter(todo => {
+      if(filter === "active") return !todo.completed
+      if(filter === "completed") return !todo.completed
+      return true
+    })
+
+  return (
+    <TodoContainer>
+          <TodoHeader></TodoHeader>
+
+          <TodoForm addTodo={addTodo}></TodoForm>
+
+          <TodoList 
+          todoList={filteredTodos} 
+          toggleTodoCompleted={toggleTodoCompleted}
+          setFilter={setFilter}
+          ></TodoList>
+    </TodoContainer>
+  )
+
+}
+
+export default App;
---
+++ src/components/TodoList/index.tsx
@@ -0,0 +1,99 @@
+import { useContext } from "react";
+import { themeConfig } from "../../contexts/theme"
+import { ThemeContext } from '../../contexts/ThemeContext';
+import type { Todo } from "../../App";
+import IconCheck from '/images/icon-check.svg'
+
+interface TodoListProps {
+  todoList: Todo[];
+  toggleTodoCompleted: (id: number) => void;
+  setFilter: (filter: 'all' | 'active' | 'completed') => void;
+}
+
+const TodoList = ({ todoList, toggleTodoCompleted, setFilter }: TodoListProps) => {
+
+  const { theme } = useContext(ThemeContext)
+
+  return (
+
+    <>
+      <div className={` ${themeConfig[theme].todo.backgroundColor} rounded-md"`}>
+        <ul>
+
+          {
+            todoList.map((todo) => (
+              <li key={todo.id} className={`p-6 border-b ${themeConfig[theme].todo.borderColor}`}>
+
+                <div className="flex items-center gap-4">
+                  <span className="w-6 h-6 rounded-full hover:bg-[linear-gradient(to_right,hsl(192,100%,67%),hsl(280,87%,65%))]  hover:p-[1px]">
+                    <button
+                      onClick={() => toggleTodoCompleted(todo.id)}
+                      className={`w-full h-full border ${themeConfig[theme].todo.borderColor} rounded-full cursor-pointer ${themeConfig[theme].todo.backgroundColor} ${todo.completed ? 'bg-[linear-gradient(to_right,hsl(192,100%,67%),hsl(280,87%,65%))]' : ''}`}>
+                      {todo.completed && (<img src={IconCheck} alt="check" className="h-2 w-2 m-auto" />)}
+                    </button>
+                  </span>
+
+                  <p className={`${themeConfig[theme].todo.textColor} ${todo.completed ? 'line-through opacity-50' : ""}`}>{todo.text}</p>
+                </div>
+              </li>
+            ))
+          }
+
+        </ul>
+
+        
+
+
+          <div className={`text-sm flex justify-between p-4 ${themeConfig[theme].layout.textColor}`}>
+
+            {todoList.length > 0 && (
+            <p>{todoList.length} items total</p>
+            )}
+            <div className="hidden sm:flex gap-4">
+              
+
+              <button onClick={() => setFilter("all")}
+                className={`text-bright-blue cursor-pointer 
+                ${theme === "dark" ? "hover:text-neutral-light-grayish-blue-hover" : "hover:text-neutral-very-dark-grayish-blue"}`}>
+                  All</button>
+              <button onClick={() => setFilter("active")} 
+              className={`cursor-pointer ${theme === "dark" ? "hover:text-neutral-light-grayish-blue-hover" : "hover:text-neutral-very-dark-grayish-blue"}`}>
+                Active</button>
+              <button onClick={() => setFilter("completed")} 
+              className={`cursor-pointer ${theme === "dark" ? "hover:text-neutral-light-grayish-blue-hover" : "hover:text-neutral-very-dark-grayish-blue"}`}>
+                Complete</button>
+
+            </div>
+
+            <button className={`${theme === "dark" ? "hover:text-neutral-light-grayish-blue-hover" : "hover:text-neutral-very-dark-grayish-blue"}`}>Clear Completed</button>
+          </div>
+
+        
+
+      </div>
+
+      
+
+        <div className={`${themeConfig[theme].todo.backgroundColor} ${themeConfig[theme].layout.textColor} flex justify-center gap-5 py-4 rounded-md mt-4 sm:hidden `}>
+          <button onClick={() => setFilter("all")} 
+          className={`text-bright-blue cursor-pointer 
+          ${theme === "dark" ? "hover:text-neutral-light-grayish-blue-hover" : "hover:text-neutral-very-dark-grayish-blue"}`}>
+            All</button>
+          <button onClick={() => setFilter("active")} 
+          className={`cursor-pointer 
+          ${theme === "dark" ? "hover:text-neutral-light-grayish-blue-hover" : "hover:text-neutral-very-dark-grayish-blue"}`}>
+            Active</button>
+          <button onClick={() => setFilter("completed")} 
+          className={`cursor-pointer 
+          ${theme === "dark" ? "hover:text-neutral-light-grayish-blue-hover" : "hover:text-neutral-very-dark-grayish-blue"}`}>
+            Complete</button>
+        </div>
+
+     
+
+    </>
+
+  )
+}
+
+export default TodoList
